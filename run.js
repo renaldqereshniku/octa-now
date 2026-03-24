726c83bdd55566c5ae114360b5e8a0ab567a2327
Renald Q <renaldq@icloud.com>
2026-03-24 12:37:05 -0700

    Update run.js

--- run.js
+++ run.js
@@ -1,54 +1,59 @@
-var one = '1';
-var two = '2';
-var three = '3';
-var four = '4';
-var five = '5';
-var six = '6';
-var seven = '7';
-var eight = '8';
+var numberZero = '0';
+var numberOne = '1';
+var numberTwo = '2';
+var numberThree = '3';
+var numberFour = '4';
+var numberFive = '5';
+var numberSix = '6';
+var numberSeven = '7';
+var numberEight = '8';
+var numberNine = '9';
 
-var firstNumberArray = [one, two, three, four, five, six, seven, eight];
+var firstDigitArray = [numberZero, numberOne, numberTwo, numberThree, numberFour, numberFive, numberSix, numberSeven, numberEight];
 
-var firstNumber = document.getElementById('firstNumber');
+var firstDigit = document.getElementById('firstDigit');
 
-firstNumber.innerHTML = firstNumberArray[0];
+firstDigit.innerHTML = firstDigitArray[0];
 
 var i = 0;
 
-function step (event) {
+function step(event) {
     if (i === 0) {
-        firstNumber.innerHTML = firstNumberArray[0];
+        firstDigit.innerHTML = firstDigitArray[0];
         i = 1;
     }
     else if (i === 1) {
-        firstNumber.innerHTML = firstNumberArray[1];
+        firstDigit.innerHTML = firstDigitArray[1];
         i = 2;
     }
     else if (i === 2) {
-        firstNumber.innerHTML = firstNumberArray[2];
+        firstDigit.innerHTML = firstDigitrArray[2];
         i = 3;
     }
     else if (i === 3) {
-        firstNumber.innerHTML = firstNumberArray[3];
+        firstDigit.innerHTML = firstDigitrArray[3];
         i = 4;
     }
     else if (i === 4) {
-        firstNumber.innerHTML = firstNumberArray[4];
+        firstDigit.innerHTML = firstDigitArray[4];
         i = 5;
     }
     else if (i === 5) {
-        firstNumber.innerHTML = firstNumberArray[5];
+        firstDigit.innerHTML = firstDigitArray[5];
         i = 6;
     }
     else if (i === 6) {
-        firstNumber.innerHTML = firstNumberArray[6];
+        firstDigit.innerHTML = firstDigitArray[6];
         i = 7;
     }
     else if (i === 7) {
-        firstNumber.innerHTML = firstNumberArray[7];
-        i = 0;
+        firstDigit.innerHTML = firstDigitArray[7];
+        i = 8;
+    }
+    else if (i === 8) {
+        firstDigit.innerHTML = firstDigitArray[8];
+        i = 1;
     }
 }
 
-setInterval(step, 700);
-
+setInterval(step, 888);
